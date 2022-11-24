5bf08978dca58f900c294ca3b3853d13e89ffd0e
Stefan Temme <temme@objectcode.de>
2022-11-25 00:45:19 +0100

    swap position

--- themes/catch-base-child/functions.php
+++ themes/catch-base-child/functions.php
@@ -1,34 +1,44 @@
 <?php
+
 namespace CatchBaseChild;
 
-function enqueCustomStyle(){
-    $parenthandle = 'catchbase-style';
-    $theme        = wp_get_theme();
-    wp_enqueue_style( $parenthandle,
-        get_template_directory_uri() . '/style.css',
-        array(),
-        $theme->parent()->get( 'Version' )
-    );
-    wp_enqueue_style( 'catchbase-child-style',
-        get_stylesheet_uri(),
-        array( $parenthandle ),
-        $theme->get( 'Version' )
-    );
+function enqueCustomStyle() {
+	$parenthandle = 'catchbase-style';
+	$theme        = wp_get_theme();
+	wp_enqueue_style( $parenthandle,
+		get_template_directory_uri() . '/style.css',
+		array(),
+		$theme->parent()->get( 'Version' )
+	);
+	wp_enqueue_style( 'catchbase-child-style',
+		get_stylesheet_uri(),
+		array( $parenthandle ),
+		$theme->get( 'Version' )
+	);
 }
 
 
 \add_action( 'wp_enqueue_scripts', 'CatchBaseChild\enqueCustomStyle' );
 
-function giveParentCredit($content){
-	$theme_data = wp_get_theme()->parent();
-	$content['right'].=' '.esc_attr( $theme_data->get( 'Name') ) . '&nbsp;' . __( 'by', 'catch-base' ). '&nbsp;<a target="_blank" href="'. esc_url( $theme_data->get( 'AuthorURI' ) ) .'">'. esc_attr( $theme_data->get( 'Author' ) ) .'</a>';
+function giveParentCredit( $content ) {
+	$theme_data       = wp_get_theme()->parent();
+	$content['right'] = esc_attr( $theme_data->get( 'Name' ) )
+	                    . '&nbsp;' . __( 'by', 'catch-base' )
+	                    . '&nbsp;<a target="_blank" href="'
+	                    . esc_url( $theme_data->get( 'AuthorURI' ) )
+	                    . '">' . esc_attr( $theme_data->get( 'Author' ) )
+	                    . '</a>'
+	                    . '<br/>'
+	                    . $content['right'];
+
 	return $content;
 }
 
 
-function setupCustomHooks(){
+function setupCustomHooks() {
 	delete_transient( 'catchbase_footer_content' );
-	\add_filter( 'catchbase_get_content','CatchBaseChild\giveParentCredit');
+	\add_filter( 'catchbase_get_content', 'CatchBaseChild\giveParentCredit' );
 }
-\add_action('catchbase_footer','CatchBaseChild\setupCustomHooks');
+
+\add_action( 'catchbase_footer', 'CatchBaseChild\setupCustomHooks' );
 
