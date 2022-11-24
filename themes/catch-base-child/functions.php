fbec1276907c95b8bd1c47897462e112a9d2c621
Stefan Temme <temme@objectcode.de>
2022-11-24 22:55:08 +0100

    working

--- themes/catch-base-child/functions.php
+++ themes/catch-base-child/functions.php
@@ -2,10 +2,17 @@
 namespace CatchBaseChild;
 
 function enqueCustomStyle(){
+    $parenthandle = 'catchbase-style';
+    $theme        = wp_get_theme();
+    wp_enqueue_style( $parenthandle,
+        get_template_directory_uri() . '/style.css',
+        null,
+        date( 'Ymd-Gis', filemtime( get_template_directory() . '/style.css' ) )
+    );
     wp_enqueue_style( 'catchbase-child-style',
         get_stylesheet_uri(),
-        array( 'catchbase-style' ),
-        wp_get_theme()->get( 'Version' )
+        array( $parenthandle ),
+        date( 'Ymd-Gis',filemtime(get_stylesheet_directory().'/style.css'))
     );
 }
 
